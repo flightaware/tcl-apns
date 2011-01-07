0e640ae115b8479189fa7f331ab5bed7f340e0a2
Jeff Lawson <jeff@bovine.net>
2011-01-07 12:08:55 -0600

    update BSD licensing details

--- apns.tcl
+++ apns.tcl
@@ -1,3 +1,34 @@
+#
+# Copyright (c) 2010-2011, FlightAware, LLC
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#
+#     * Redistributions in binary form must reproduce the above
+#       copyright notice, this list of conditions and the following
+#       disclaimer in the documentation and/or other materials provided
+#       with the distribution.
+#
+#     * Neither the name of FlightAware, LLC nor the names of its
+#       contributors may be used to endorse or promote products derived
+#       from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 package require tls
 
@@ -310,22 +341,4 @@ proc connect_and_receive_apns_feedback {} {
 
 # --------------------------------------------------
 # --------------------------------------------------
-
-# proc debugging {} {
-#     for {set i 0} {$i < 10} {incr i} {
-#         set json "{\"aps\": \"hello message $i\"}"
-#         send_apns "123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF$i" $json
-#         puts "sent"
-#         after 1000
-#         puts "looping"
-#     }
-#     puts "Sleeping 10"
-#     after 5000
-#     connect_and_receive_apns_feedback
-#     if {[llength $::apns(bad_device_tokens)] > 0} {
-#         puts "have [llength $::apns(bad_device_tokens)] waiting device_tokens"
-#     } else {
-#         puts "no bad_device_tokens waiting"
-#     }
-# }
 
