fb60ff343a4cb3ef591e60606553883a9475d9d8
Jeff Lawson <jeff@bovine.net>
2011-01-12 01:15:10 -0600

    use a namespace to contain all of our stuff

--- apns.tcl
+++ apns.tcl
@@ -32,31 +32,49 @@
 
 package require tls
 
-# set ::apns(connect_timeout_ms) 5000
-# set ::apns(write_timeout_ms) 2000
-# set ::apns(feedback_timeout_ms) 30000
-# set ::apns(feedback_connect_period) 600
 
-# set ::apns(gateway_host)  gateway.push.apple.com
-# set ::apns(gateway_port)  2195
+namespace eval ::apns {
 
-# set ::apns(feedback_host) feedback.push.apple.com
-# set ::apns(feedback_port) 2196
+    # timeouts for network operations
+    variable connect_timeout_ms 5000
+    variable write_timeout_ms 2000
+    variable feedback_timeout_ms 30000
 
-# set ::apns(certificate)   apns-production.crt
-# set ::apns(private_key)   apns-production.key
+    # connection details for the gateway server
+    variable gateway_host  "gateway.push.apple.com"
+    variable gateway_port  2195
+
+    # connection details for the feedback server
+    variable feedback_host  "feedback.push.apple.com"
+    variable feedback_port  2196
+
+    # filenames of your personal certificate and private-key
+    variable certificate   "certificate.pem"
+    variable private_key   "private_key.pem"
+
+    # ------
+
+    variable gateway_chan
+    variable gateway_signal
+    variable feedback_chan
+    variable feedback_readbuf
+    variable feedback_signal
+    variable bad_device_tokens
 
 # --------------------------------------------------
 # --------------------------------------------------
 
+# Close the gateway socket.
 proc close_apns_gateway {} {
+    variable gateway_chan
     logmsg "Closing gateway connection..."
-    if {[info exists ::apns(gateway_chan)]} {
-        catch { close $::apns(gateway_chan) }
-        unset -nocomplain ::apns(gateway_chan)
+    if {[info exists gateway_chan]} {
+        catch { close $gateway_chan }
+        unset -nocomplain gateway_chan
     }
 }
 
+# Helper callback used to handle reading data when the socket becomes readable.
 proc readable_callback_apns_drain {chan signame} {
     if {[eof $chan]} {
         fileevent $chan readable {}
@@ -69,13 +87,14 @@ proc readable_callback_apns_drain {chan signame} {
     if {[string length $drained] != 0} {
         fileevent $chan readable {}
         fileevent $chan writable {}
-        logerr "readable_callback_apns_drain: Read unexpected: {$drained}"
+        logerr "readable_callback_apns_drain: Read unexpected: $drained"
         set $signame -1
         return
     }
     set $signame 1
 }
 
+# Helper callback used to handle sending data when the socket becomes writable.
 proc writable_callback_apns_handshake {chan signame} {
     if {[catch {tls::handshake $chan} result]} {
         fileevent $chan readable {}
@@ -96,6 +115,8 @@ proc writable_callback_apns_handshake {chan signame} {
     }
 }
 
+
+# Helper callback used to handle sending data when the socket becomes writable.
 proc writable_callback_apns_transmit {chan signame payload} {
     fileevent $chan writable {}
     if {[catch {puts -nonewline $chan $payload; flush $chan} result]} {
@@ -108,6 +129,7 @@ proc writable_callback_apns_transmit {chan signame payload} {
 }
 
 
+# Helper callback used when a read or write operations takes too long.
 proc timeout_callback_apns {chan signame} {
     logerr "timeout_callback_apns: Timeout occurred"
     set $signame -1
@@ -116,42 +138,54 @@ proc timeout_callback_apns {chan signame} {
 }
 
 
+# Open a connection to the gateway server
 # Returns 0 on error.
 proc connect_apns_gateway {} {
-    logmsg "Opening secure gateway connection to $::apns(gateway_host):$::apns(gateway_port)..."
-    set ::apns(gateway_chan)  [tls::socket -async -certfile $::apns(certificate) -keyfile $::apns(private_key) $::apns(gateway_host) $::apns(gateway_port)]
+    variable gateway_chan
+    variable gateway_signal
+    variable gateway_host
+    variable gateway_port
+    variable certificate
+    variable private_key
+    variable connect_timeout_ms
+
+    logmsg "Opening secure gateway connection to $gateway_host:$gateway_port ..."
+    set gateway_chan  [tls::socket -async -certfile $certificate -keyfile $private_key $gateway_host $gateway_port]
 
     # start in binary/blocking mode until the SSL negotiation is finished.
-    fconfigure $::apns(gateway_chan) -encoding binary -buffering none -blocking 1
+    fconfigure $gateway_chan -encoding binary -buffering none -blocking 1
 
     # wait for the negotiation to complete or timeout.
-    set ::apns(gateway_signal) 0
-    fileevent $::apns(gateway_chan) writable [list writable_callback_apns_handshake $::apns(gateway_chan) ::apns(gateway_signal)]
-    fileevent $::apns(gateway_chan) readable [list readable_callback_apns $::apns(gateway_chan) ::apns(gateway_signal)]
-    set afterID [after $::apns(connect_timeout_ms) timeout_callback_apns $::apns(gateway_chan) ::apns(gateway_signal)]
-    vwait ::apns(gateway_signal)
+    set ::apns::gateway_signal 0
+    fileevent $gateway_chan writable [list writable_callback_apns_handshake $gateway_chan ::apns::gateway_signal]
+    fileevent $gateway_chan readable [list readable_callback_apns $gateway_chan ::apns::gateway_signal]
+    set afterID [after $connect_timeout_ms timeout_callback_apns $gateway_chan ::apns::gateway_signal]
+    vwait ::apns::gateway_signal
 
     after cancel $afterID
-    fileevent $::apns(gateway_chan) readable {}
-    fileevent $::apns(gateway_chan) writable {}
+    fileevent $gateway_chan readable {}
+    fileevent $gateway_chan writable {}
 
-    #puts "connect_apns_gateway: done connecting, status $::apns(gateway_signal)"
+    #puts "connect_apns_gateway: done connecting, status $::apns::gateway_signal"
 
-    if {$::apns(gateway_signal) < 0} {
+    if {$::apns::gateway_signal < 0} {
         close_apns_gateway
         return 0
     }
 
     # switch to non-blocking mode.
-    fconfigure $::apns(gateway_chan) -blocking 0
+    fconfigure $gateway_chan -blocking 0
     return 1
 }
 
 
-# Returns 0 on error.
+# Connect to the gateway server, but only if not already connected
+# Returns 0 on error, 1 on success (already connected or connect succeeded).
 proc connect_apns_gateway_ifneeded {} {
+    variable gateway_chan
+
     # if no socket, then try to open one.
-    if {![info exists ::apns(gateway_chan)]} {
+    if {![info exists gateway_chan]} {
         return [connect_apns_gateway]
     }
 
@@ -162,6 +196,9 @@ proc connect_apns_gateway_ifneeded {} {
 # Send an alert to the gateway service.
 # Throws error on transmit failure.
 proc send_apns {deviceToken payload} {
+    variable gateway_chan
+    variable gateway_signal
+    variable write_timeout_ms
 
     # make sure it isn't too long.
     if {[string length $deviceToken] > 64 || [string length $deviceToken] % 2 != 0 || [string length $deviceToken] == 0} {
@@ -196,19 +233,19 @@ proc send_apns {deviceToken payload} {
             }
 
             # wait for the  to complete or timeout.
-            set ::apns(gateway_signal) 0
-            fileevent $::apns(gateway_chan) writable [list writable_callback_apns_transmit $::apns(gateway_chan) ::apns(gateway_signal) $msg]
-            fileevent $::apns(gateway_chan) readable [list readable_callback_apns_drain $::apns(gateway_chan) ::apns(gateway_signal)]
-            set afterID [after $::apns(write_timeout_ms) timeout_callback_apns $::apns(gateway_chan) ::apns(gateway_signal)]
-            vwait ::apns(gateway_signal)
+            set ::apns::gateway_signal 0
+            fileevent $gateway_chan writable [list writable_callback_apns_transmit $gateway_chan ::apns::gateway_signal $msg]
+            fileevent $gateway_chan readable [list readable_callback_apns_drain $gateway_chan ::apns::gateway_signal]
+            set afterID [after $write_timeout_ms timeout_callback_apns $gateway_chan ::apns::gateway_signal)]
+            vwait ::apns::gateway_signal
 
             # cleanup after waiting.
             after cancel $afterID
-            fileevent $::apns(gateway_chan) readable {}
-            fileevent $::apns(gateway_chan) writable {}
+            fileevent $gateway_chan readable {}
+            fileevent $gateway_chan writable {}
 
-            if {$::apns(gateway_signal) != 1} {
-                error "send_apns: send failure, got $::apns(gateway_signal)"
+            if {$::apns::gateway_signal != 1} {
+                error "send_apns: send failure, got $::apns::gateway_signal"
             }
         } result] == 1} {
             logerr "send_apns: Error occurred: $result"
@@ -226,16 +263,21 @@ proc send_apns {deviceToken payload} {
 # --------------------------------------------------
 # --------------------------------------------------
 
+# Close the feedback socket.
 proc close_apns_feedback {} {
+    variable feedback_chan
     logmsg "Closing feedback connection..."
-    if {[info exists ::apns(feedback_chan)]} {
-        catch { close $::apns(feedback_chan) }
-        unset -nocomplain ::apns(feedback_chan)
+    if {[info exists feedback_chan]} {
+        catch { close $feedback_chan }
+        unset -nocomplain feedback_chan
     }
 }
 
 
+# Helper callback used to read data when the feedback socket becomes readable.
 proc readable_callback_apns_feedback {chan signame} {
+    variable feedback_readbuf
+
     if {[eof $chan]} {
         logmsg "readable_callback_apns_feedback: Closed secure network feedback connection due to EOF";
         fileevent $chan readable {}
@@ -243,14 +285,14 @@ proc readable_callback_apns_feedback {chan signame} {
         set $signame 1
         return
     }
-    if {[string length $::apns(feedback_readbuf)] < 6} {
+    if {[string length $feedback_readbuf] < 6} {
         # read enough to interpret the header
-        set readamt [expr 6-[string length $::apns(feedback_readbuf)]]
-        append ::apns(feedback_readbuf) [read $chan $readamt]
+        set readamt [expr 6-[string length $feedback_readbuf]]
+        append feedback_readbuf [read $chan $readamt]
     }
-    if {[string length $::apns(feedback_readbuf)] >= 6} {
+    if {[string length $feedback_readbuf] >= 6} {
         # parse the header and compute the total message size.
-        if {[binary scan $::apns(feedback_readbuf) "IuSu" timestamp tokenlen] != 2} {
+        if {[binary scan $feedback_readbuf "IuSu" timestamp tokenlen] != 2} {
             logerr "readable_callback_apns_feedback: Parse failure reading secure network connection";
             fileevent $chan readable {}
             fileevent $chan writable {}
@@ -258,19 +300,19 @@ proc readable_callback_apns_feedback {chan signame} {
             return
         }
         set msgsize [expr 6+$tokenlen]
-        #puts "debug: Whole message size will be $msgsize (have [string length $::apns(feedback_readbuf)] now)"
+        #puts "debug: Whole message size will be $msgsize (have [string length $feedback_readbuf] now)"
 
         # read enough to get the entire message
-        if {[string length $::apns(feedback_readbuf)] < $msgsize} {
-            set readamt [expr $msgsize-[string length $::apns(feedback_readbuf)]]
-            append ::apns(feedback_readbuf) [read $chan $readamt]
+        if {[string length $feedback_readbuf] < $msgsize} {
+            set readamt [expr $msgsize-[string length $feedback_readbuf]]
+            append feedback_readbuf [read $chan $readamt]
         }
 
         # if we have an entire message, then parse it.
-        if {[string length $::apns(feedback_readbuf)] >= $msgsize} {
+        if {[string length $feedback_readbuf] >= $msgsize} {
             set formatstr [format "IuSuH%da*" [expr 2*$tokenlen]]
-            if {[binary scan $::apns(feedback_readbuf) $formatstr timestamp tokenlen device_token ::apns(feedback_readbuf)] != 4} {
-                logerr "readable_callback_apns_feedback: Parse failure reading secure network connection";
+            if {[binary scan $feedback_readbuf $formatstr timestamp tokenlen device_token feedback_readbuf] != 4} {
+                logerr "readable_callback_apns_feedback: Parse failure reading secure network connection"
                 fileevent $chan readable {}
                 fileevent $chan writable {}
                 set $signame -1
@@ -279,43 +321,55 @@ proc readable_callback_apns_feedback {chan signame} {
 
             # excellent, we have parsed an entire message.
             logmsg "Got bounce feedback [clock format $timestamp -format {%Y-%m-%d %H:%M:%S} -timezone :UTC] for $device_token"
-            if {[lsearch -exact $::apns(bad_device_tokens) $device_token] == -1} {
-                lappend ::apns(bad_device_tokens) $device_token
+            if {[lsearch -exact $bad_device_tokens $device_token] == -1} {
+                lappend bad_device_tokens $device_token
             }
         }
     }
 }
 
 
+# Connect to the feedback server, reading any responses, and then disconnect.
+# Appends the responses to ::apns::bad_device_tokens
 # Returns 0 on error.
 proc connect_and_receive_apns_feedback {} {
-    if {![info exists ::apns(bad_device_tokens)]} {
-        set ::apns(bad_device_tokens) {}
+    variable bad_device_tokens
+    variable feedback_chan
+    variable feedback_readbuf
+    variable feedback_host
+    variable feedback_port
+    variable certificate
+    variable private_key 
+    variable connect_timeout_ms
+    variable feedback_timeout_ms
+
+    if {![info exists bad_device_tokens]} {
+        set bad_device_tokens {}
     }
 
     # open new connection
-    logmsg "Opening secure feedback connection to $::apns(feedback_host):$::apns(feedback_port)..."
-    set ::apns(feedback_chan)  [tls::socket -async -certfile $::apns(certificate) -keyfile $::apns(private_key) $::apns(feedback_host) $::apns(feedback_port)]
+    logmsg "Opening secure feedback connection to $feedback_host:$feedback_port ..."
+    set feedback_chan  [tls::socket -async -certfile $certificate -keyfile $private_key $feedback_host $feedback_port]
 
     # start in binary/blocking mode until the SSL negotiation is finished.
-    fconfigure $::apns(feedback_chan) -encoding binary -buffering none -blocking 1
+    fconfigure $feedback_chan -encoding binary -buffering none -blocking 1
 
     # wait for the negotiation to complete or timeout.
-    set ::apns(feedback_signal) 0
-    set ::apns(feedback_readbuf) {}
-    fileevent $::apns(feedback_chan) writable [list writable_callback_apns_handshake $::apns(feedback_chan) ::apns(feedback_signal)]
-    fileevent $::apns(feedback_chan) readable [list readable_callback_apns_feedback $::apns(feedback_chan) ::apns(feedback_signal)]
-    set afterID [after $::apns(connect_timeout_ms) [list timeout_callback_apns $::apns(feedback_chan) ::apns(feedback_signal)]]
+    set ::apns::feedback_signal 0
+    set feedback_readbuf {}
+    fileevent $feedback_chan writable [list writable_callback_apns_handshake $feedback_chan ::apns::feedback_signal]
+    fileevent $feedback_chan readable [list readable_callback_apns_feedback $feedback_chan ::apns::feedback_signal]
+    set afterID [after $connect_timeout_ms [list timeout_callback_apns $feedback_chan ::apns::feedback_signal]]
     logmsg "Waiting for feedback_signal"
-    vwait ::apns(feedback_signal)
+    vwait ::apns::feedback_signal
 
     after cancel $afterID
-    fileevent $::apns(feedback_chan) readable {}
-    fileevent $::apns(feedback_chan) writable {}
+    fileevent $feedback_chan readable {}
+    fileevent $feedback_chan writable {}
 
     logmsg "Finished waiting"
 
-    if {$::apns(feedback_signal) < 0} {
+    if {$::apns::feedback_signal < 0} {
         close_apns_feedback
         return 0
     }
@@ -323,16 +377,16 @@ proc connect_and_receive_apns_feedback {} {
     logmsg "Reading from feedback"
 
     # switch to non-blocking mode and wait until timeout or EOF.
-    set ::apns(feedback_signal) 0
-    fconfigure $::apns(feedback_chan) -blocking 0
-    fileevent $::apns(feedback_chan) readable [list readable_callback_apns_feedback $::apns(feedback_chan) ::apns(feedback_signal)]
-    set afterID [after $::apns(feedback_timeout_ms) [list timeout_callback_apns $::apns(feedback_chan) ::apns(feedback_signal)]]
-    vwait ::apns(feedback_signal)
+    set ::apns::feedback_signal 0
+    fconfigure $feedback_chan -blocking 0
+    fileevent $feedback_chan readable [list readable_callback_apns_feedback $feedback_chan ::apns::feedback_signal]
+    set afterID [after $feedback_timeout_ms [list timeout_callback_apns $feedback_chan ::apns::feedback_signal]]
+    vwait ::apns::feedback_signal
 
     # done with connection so close it
     close_apns_feedback
 
-    if {$::apns(feedback_signal) < 0} {
+    if {$::apns::feedback_signal < 0} {
         return 0
     }
     return 1
@@ -342,3 +396,4 @@ proc connect_and_receive_apns_feedback {} {
 # --------------------------------------------------
 # --------------------------------------------------
 
+}
--- example.tcl
+++ example.tcl
@@ -4,33 +4,33 @@
 source apns.tcl
 
 
-set ::apns(connect_timeout_ms) 5000
-set ::apns(write_timeout_ms) 2000
-set ::apns(feedback_timeout_ms) 30000
-set ::apns(feedback_connect_period) 600
+# If you need to change the timeout parameters, you can uncomment the following...
+#set ::apns::connect_timeout_ms 5000
+#set ::apns::write_timeout_ms 2000
+#set ::apns::feedback_timeout_ms 30000
 
 if {1} {
     # for development, use Apple's sandbox servers.
     # Messages will still get delivered, but Apple is less paranoid about misbehaving or abusive 
-    set ::apns(gateway_host)  gateway.sandbox.push.apple.com
-    set ::apns(gateway_port)  2195
+    set ::apns::gateway_host  gateway.sandbox.push.apple.com
+    set ::apns::gateway_port  2195
 
-    set ::apns(feedback_host) feedback.sandbox.push.apple.com
-    set ::apns(feedback_port) 2196
+    set ::apns::feedback_host feedback.sandbox.push.apple.com
+    set ::apns::feedback_port 2196
 
-    set ::apns(certificate)   apns-sandbox.crt
-    set ::apns(private_key)   apns-sandbox.key
+    set ::apns::certificate   apns-sandbox.crt
+    set ::apns::private_key   apns-sandbox.key
 
 } else {
     # for production, use Apple's real servers.
-    set ::apns(gateway_host)  gateway.push.apple.com
-    set ::apns(gateway_port)  2195
+    set ::apns::gateway_host  gateway.push.apple.com
+    set ::apns::gateway_port  2195
     
-    set ::apns(feedback_host) feedback.push.apple.com
-    set ::apns(feedback_port) 2196
+    set ::apns::feedback_host feedback.push.apple.com
+    set ::apns::feedback_port 2196
     
-    set ::apns(certificate)   apns-production.crt
-    set ::apns(private_key)   apns-production.key
+    set ::apns::certificate   apns-production.crt
+    set ::apns::private_key   apns-production.key
 }
 
 # A simple logging proc.
@@ -48,28 +48,30 @@ proc logerr {msg} {
 
 proc main {} {
 
-    # send a message to a few arbitrary deviceTokens.
-    # In a real service, this would probably involve a SELECT from your database.
-    for {set i 0} {$i < 10} {incr i} {
-        set json "{\"aps\": \"hello message $i\"}"
-        set deviceToken "123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF$i"
-        send_apns $deviceToken $json
-        logmsg "sent"
-        after 1000
-        logmsg "looping"
-    }
-    
-    # wait for a little bit for the Feedback server.
-    logmsg "Sleeping..."
-    after 5000
-
-    # check with the Feedback server to see if there were any bounced devices.
-    # In a real service, you would mark the devices as bad in your database and not send future messages.
-    connect_and_receive_apns_feedback
-    if {[llength $::apns(bad_device_tokens)] > 0} {
-        logmsg "have [llength $::apns(bad_device_tokens)] waiting device_tokens"
-    } else {
-        logmsg "no bad_device_tokens waiting"
+    while 1 {
+        # send a message to a few arbitrary deviceTokens.
+        # In a real service, this would probably involve a SELECT from your database.
+        for {set i 0} {$i < 10} {incr i} {
+            set json "{\"aps\": \"hello message $i\"}"
+            set deviceToken "123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF$i"
+            ::apns::send_apns $deviceToken $json
+            logmsg "sent"
+            after 1000
+            logmsg "looping"
+        }
+        
+        # wait for a little bit for the Feedback server.
+        logmsg "Sleeping..."
+        after 5000
+        
+        # check with the Feedback server to see if there were any bounced devices.
+        # In a real service, you would mark the devices as bad in your database and not send future messages.
+        ::apns::connect_and_receive_apns_feedback
+        if {[llength $::apns::bad_device_tokens] > 0} {
+            logmsg "have [llength $::apns::bad_device_tokens] waiting device_tokens"
+        } else {
+            logmsg "no bad_device_tokens waiting"
+        }
     }
 }
 
